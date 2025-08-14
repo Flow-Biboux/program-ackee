06a7c1bc8125b841c3ced942081c667d57ed8c67
flow_biboux <florianesm@gmail.com>
2025-08-14 19:06:53 +0700

    yoyo

--- frontend/src/components/dashboard/dashboard-feature.tsx
+++ frontend/src/components/dashboard/dashboard-feature.tsx
@@ -14,7 +14,7 @@ export function DashboardFeature() {
       <AppHero title="gm" subtitle="Say hi to your new Solana app." />
       <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div className="space-y-2">
-          <p>Here are some helpful links to get you started.</p>
+          <p>Here are some helpful links to get you started. yoyo</p>
           {links.map((link, index) => (
             <div key={index}>
               <a
